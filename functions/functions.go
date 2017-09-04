6c8af3288a86ea5a8c087f5e5c4fbdb83d998390
Pengzhi Sun <pengzhisun@live.com>
2017-09-04 18:18:02 +0800

    add functions tour

---
+++ functions/functions.go
@@ -0,0 +1,25 @@
+/* ****************************************************************************
+ * Tour: Functions
+ * Link: https://tour.golang.org/basics/4
+ * ----------------------------------------------------------------------------
+ * A function can take zero or more arguments.
+ *
+ * In this example, add takes two parameters of type int.
+ *
+ * Notice that the type comes after the variable name.
+ *
+ * For more about why types look the way they do, see the article on Go's
+ * declaration syntax: https://blog.golang.org/gos-declaration-syntax.
+ * ***************************************************************************/
+
+package main
+
+import "fmt"
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(add(42, 13))
+}
