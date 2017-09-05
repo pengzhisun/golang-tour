3720d45a904ce2a2bb0560377e9a1c868a33c379
Pengzhi Sun <pengzhisun@live.com>
2017-09-05 18:38:43 +0800

    add constants tour

---
+++ constants/constants.go
@@ -0,0 +1,24 @@
+/* ****************************************************************************
+ * Tour: Constants
+ * Link: https://tour.golang.org/basics/15
+ * ----------------------------------------------------------------------------
+ * Constants are declared like variables, but with the const keyword.
+ * Constants can be character, string, boolean, or numeric values.
+ * Constants cannot be declared using the := syntax.
+ * ***************************************************************************/
+
+package main
+
+import "fmt"
+
+// Pi constants
+const Pi = 3.14
+
+func main() {
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+}
