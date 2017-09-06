f1fcf982cfb6e20d127462fe5ad16eed71a08e72
Pengzhi Sun <pengzhisun@live.com>
2017-09-06 16:48:43 +0800

    add parent directory argument to run_tour script

--- run_tour.sh
+++ run_tour.sh
@@ -1,9 +1,10 @@
 #!/bin/env bash
 
 TOUR_NAME=$1
+PARENT_DIR=${2:-.}
 
 pushd . > /dev/null
-cd ${TOUR_NAME} > /dev/null
+cd ${PARENT_DIR}/${TOUR_NAME} > /dev/null
 go build > /dev/null
 ./${TOUR_NAME}
 popd > /dev/null
