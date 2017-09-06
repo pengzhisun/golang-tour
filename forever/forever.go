342178197da996b0fb5c44f9e43b975f515f4c3e
Pengzhi Sun <pengzhisun@live.com>
2017-09-06 12:19:35 +0800

    add forever tour

---
+++ forever/forever.go
@@ -0,0 +1,14 @@
+/* ****************************************************************************
+ * Tour: Forever
+ * Link: https://tour.golang.org/flowcontrol/4
+ * ----------------------------------------------------------------------------
+ * If you omit the loop condition it loops forever, so an infinite loop is
+ * compactly expressed.
+ * ***************************************************************************/
+
+package main
+
+func main() {
+	for {
+	}
+}
