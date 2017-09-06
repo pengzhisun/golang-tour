4e9d173831ac505e606c397ab5025e20e91f0f85
Pengzhi Sun <pengzhisun@live.com>
2017-09-06 12:13:06 +0800

    add new tour helper script

---
+++ new_tour.sh
@@ -0,0 +1,22 @@
+#!/bin/env bash
+# usages:
+#    bash new_tourh.sh for flowcontrol/1
+
+TOUR_NAME=$1
+RELAVITE_LINK=$2
+
+mkdir "${TOUR_NAME}"
+
+echo "/* ****************************************************************************
+ * Tour: ${TOUR_NAME}
+ * Link: https://tour.golang.org/${RELAVITE_LINK}
+ * ----------------------------------------------------------------------------
+ * 
+ * ***************************************************************************/
+
+package main
+
+import "fmt"
+
+func main() {
+}" > "${TOUR_NAME}\${TOUR_NAME}.go"
