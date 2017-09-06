6f59eddb4980af5236393b0dfea0b2de32279453
Pengzhi Sun <pengzhisun@live.com>
2017-09-06 15:28:44 +0800

    add

---
+++ if-with-a-short-statement/if-with-a-short-statement.go
@@ -0,0 +1,31 @@
+/* ****************************************************************************
+ * Tour: If with a short statement
+ * Link: https://tour.golang.org/flowcontrol/6
+ * ----------------------------------------------------------------------------
+ * Like for, the if statement can start with a short statement to execute
+ * before the condition.
+ *
+ * Variables declared by the statement are only in scope until the end of the
+ * if.
+ * ***************************************************************************/
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
+
+func main() {
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+}
