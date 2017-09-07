43998097d0cff4319a8e5e006ac0d2eee8fbfd3d
Pengzhi Sun <pengzhisun@live.com>
2017-09-07 15:51:47 +0800

    add

---
+++ mutating-maps/mutating-maps.go
@@ -0,0 +1,48 @@
+/* ****************************************************************************
+ * Tour: Mutating Maps
+ * Link: https://tour.golang.org/moretypes/22
+ * ----------------------------------------------------------------------------
+ * Insert or update an element in map m:
+ *   m[key] = elem
+ *
+ * Retrieve an element:
+ *   elem = m[key]
+ *
+ * Delete an element:
+ *   delete(m, key)
+ *
+ * Test that a key is present with a two-value assignment:
+ *   elem, ok = m[key]
+ *
+ * If key is in m, ok is true. If not, ok is false.
+ *
+ * If key is not in the map, then elem is the zero value for the map's element
+ * type.
+ *
+ * Note: if elem or ok have not yet been declared you could use a short
+ * declaration form:
+ *
+ * elem, ok := m[key]
+ * ***************************************************************************/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	m := make(map[string]int)
+
+	m["Answer"] = 42
+	fmt.Println("The value:", m["Answer"])
+
+	m["Answer"] = 48
+	fmt.Println("The value:", m["Answer"])
+
+	delete(m, "Answer")
+	fmt.Println("The value:", m["Answer"])
+
+	v, ok := m["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
+}
