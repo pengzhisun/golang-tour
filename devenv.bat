0260f2b4d11f69204c3e0638149b6bacf1155430
Pengzhi Sun <pengzhisun@live.com>
2017-09-07 17:33:11 +0800

    change comment in devenv.bat to rem

--- devenv.bat
+++ devenv.bat
@@ -2,23 +2,23 @@
 
 set $BASE_DIR=%~dp0
 
-:navigators
+rem navigators
 DOSKEY root=cd %BASE_DIR%
 
-:tour operations
+rem tour operations
 DOSKEY newtour=pushd . $T cd %BASE_DIR% $T bash new_tour.sh $* $T popd
 DOSKEY runtour=pushd . $T cd %BASE_DIR% $T bash run_tour.sh $* $T popd
 DOSKEY codetour=code $1\$1.go
 DOSKEY stagetour=git add $1
 
-:shortcuts
+rem shortcuts
 DOSKEY n2=notepad2 $1
 DOSKEY nct=pushd . $T cd %BASE_DIR% $T bash new_tour.sh $* $T popd $T code $1\$1.go
 DOSKEY cpt=git commit -m "$*" $T git push
 
-:devenv
+rem devenv
 DOSKEY upenv=pushd . $T cd %BASE_DIR% $T notepad2 devenv.bat $T popd
 DOSKEY ldenv=pushd . $T cd %BASE_DIR% $T devenv.bat $T popd
 
-:test
+rem test
 DOSKEY test1=echo "test1"
