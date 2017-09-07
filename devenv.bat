e441658376c89913b9239f64f86a096cea3b8fd4
Pengzhi Sun <pengzhisun@live.com>
2017-09-07 17:13:38 +0800

    fix commit message issue in devenv.bat script

--- devenv.bat
+++ devenv.bat
@@ -9,13 +9,16 @@ DOSKEY root=cd %BASE_DIR%
 DOSKEY newtour=pushd . $T cd %BASE_DIR% $T bash new_tour.sh $* $T popd
 DOSKEY runtour=pushd . $T cd %BASE_DIR% $T bash run_tour.sh $* $T popd
 DOSKEY codetour=code $1\$1.go
-DOSKEY committour=git add $1 $T git commit -m "$2"
+DOSKEY stagetour=git add $1
 
 :shortcuts
 DOSKEY n2=notepad2 $1
 DOSKEY nct=pushd . $T cd %BASE_DIR% $T bash new_tour.sh $* $T popd $T code $1\$1.go
-DOSKEY cpt=git add $1 $T git commit -m "$2" $T git push
+DOSKEY cpt=git commit -m "$*" $T git push
 
 :devenv
 DOSKEY upenv=pushd . $T cd %BASE_DIR% $T notepad2 devenv.bat $T popd
 DOSKEY ldenv=pushd . $T cd %BASE_DIR% $T devenv.bat $T popd
+
+:test
+DOSKEY test1=echo "test1"
