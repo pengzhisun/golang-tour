69d44964bbeb1c65bc135a71290adb8f142ace4f
Pengzhi Sun <pengzhisun@live.com>
2017-09-07 11:42:03 +0800

    add

---
+++ slices/slices.go
@@ -0,0 +1,24 @@
+/* ****************************************************************************
+ * Tour: Slices
+ * Link: https://tour.golang.org/moretypes/7
+ * ----------------------------------------------------------------------------
+ * An array has a fixed size. A slice, on the other hand, is a dynamically-sized,
+ * flexible view into the elements of an array. In practice, slices are much
+ * more common than arrays.
+ *
+ * The type []T is a slice with elements of type T.
+ *
+ * This expression creates a slice of the first five elements of the array a:
+ *   a[0:5]
+ * ***************************************************************************/
+
+package main
+
+import "fmt"
+
+func main() {
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+
+	var s []int = primes[1:4]
+	fmt.Println(s)
+}
