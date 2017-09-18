46a1e8e7b837a15a41b4d57eefb32d4e99375889
Pengzhi Sun <pengzhisun@live.com>
2017-09-18 15:28:03 +0800

    add equivalent binary trees exercise

---
+++ exercise-equivaient-binary-trees/exercise-equivaient-binary-trees.go
@@ -0,0 +1,94 @@
+/* ****************************************************************************
+ * Exercise: Equivalent Binary Trees
+ * Link: https://tour.golang.org/concurrency/7
+ * ----------------------------------------------------------------------------
+ * There can be many different binary trees with the same sequence of values
+ * stored at the leaves. For example, here are two binary trees storing the
+ * sequence 1, 1, 2, 3, 5, 8, 13.
+ *
+ *       3                     8
+ *     /   \                  / \
+ *   1       8               3   13
+ *  / \     / \             / \
+ * 1   2   5   13          1   5
+ *                        / \
+ *                       1   2
+ *
+ * A function to check whether two binary trees store the same sequence is
+ * quite complex in most languages. We'll use Go's concurrency and channels to
+ * write a simple solution.
+ *
+ * This example uses the tree package, which defines the type:
+ *   type Tree struct {
+ *     Left  *Tree
+ *     Value int
+ *     Right *Tree
+ *   }
+ *
+ * 1. Implement the Walk function.
+ *
+ * 2. Test the Walk function.
+ * The function tree.New(k) constructs a randomly-structured binary tree
+ * holding the values k, 2k, 3k, ..., 10k.
+ *
+ * Create a new channel ch and kick off the walker:
+ *   go Walk(tree.New(1), ch)
+ *
+ * Then read and print 10 values from the channel. It should be the numbers
+ * 1, 2, 3, ..., 10.
+ *
+ * 3. Implement the Same function using Walk to determine whether t1 and t2
+ * store the same values.
+ *
+ * 4. Test the Same function.
+ * Same(tree.New(1), tree.New(1)) should return true, and Same(tree.New(1),
+ * tree.New(2)) should return false.
+ *
+ * The documentation for Tree can be found here:
+ * https://godoc.org/golang.org/x/tour/tree
+ * ***************************************************************************/
+
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/tour/tree"
+)
+
+// Walk func
+func Walk(t *tree.Tree, ch chan int) {
+	if t.Left != nil {
+		Walk(t.Left, ch)
+	}
+
+	ch <- t.Value
+
+	if t.Right != nil {
+		Walk(t.Right, ch)
+	}
+}
+
+// Same func
+func Same(t1, t2 *tree.Tree) bool {
+	c1 := make(chan int, 10)
+	c2 := make(chan int, 10)
+
+	Walk(t1, c1)
+	Walk(t2, c2)
+
+	for i := 0; i < 10; i++ {
+		if <-c1 != <-c2 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Println(
+		Same(tree.New(1), tree.New(1)),
+		Same(tree.New(1), tree.New(2)),
+	)
+}
