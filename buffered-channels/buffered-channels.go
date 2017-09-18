06b8b89c4e43a816a6986d9692d3404cbb2f4466
Pengzhi Sun <pengzhisun@live.com>
2017-09-18 11:54:44 +0800

    add buffered channels

---
+++ buffered-channels/buffered-channels.go
@@ -0,0 +1,25 @@
+/* ****************************************************************************
+ * Tour: Buffered Channels
+ * Link: https://tour.golang.org/
+ * ----------------------------------------------------------------------------
+ * Channels can be buffered. Provide the buffer length as the second argument
+ * to make to initialize a buffered channel:
+ *   ch := make(chan int, 100)
+ *
+ * Sends to a buffered channel block only when the buffer is full. Receives
+ * block when the buffer is empty.
+  * ***************************************************************************/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ch := make(chan int, 2)
+	ch <- 1
+	ch <- 2
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+}
